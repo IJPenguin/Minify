4c97588187d0444f5a923e3fc51a8897f32c75c5
Penguin <ishanjaiswal370@gmail.com>
2024-07-11 05:09:34 +0530

    created: frontend and updated routes

--- src/Backend/server.js
+++ src/Backend/server.js
@@ -3,27 +3,35 @@ import { config } from "dotenv";
 import compression from "compression";
 import mongoose from "mongoose";
 import cors from "cors";
-const PORT = process.env.PORT || 6969;
-import User from "./Models/userModel.js";
-import Link from "./Models/linkModel.js";
 import session from "express-session";
 import cookieParser from "cookie-parser";
+import RedisStore from "connect-redis";
+import { createClient } from "redis";
 
-const app = express();
 config();
+const app = express();
+const PORT = process.env.PORT || 6969;
+const redisClient = createClient();
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING);
+redisClient.connect().catch(console.error);
+const redisStore = new RedisStore({
+	client: redisClient,
+	prefix: "myapp:",
+});
+
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
+app.use(compression());
 app.use(
 	session({
 		secret: process.env.SESSION_SECRET,
 		resave: false,
-		saveUninitialized: false,
+		saveUninitialized: true,
+		store: redisStore,
 		cookie: { maxAge: 60000 * 60 * 24 * 7 },
 	})
 );
-app.use(compression());
-app.use(cors());
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING);
 
 app.listen(PORT, () => {
 	console.log(`Server is started on http://localhost:${PORT}`);
